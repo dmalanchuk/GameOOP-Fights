99608f645b817350cb1521e56d80c0980e51621c
Daniil Malanchuck <124836521+dmalanchuk@users.noreply.github.com>
2025-01-09 21:03:56 +0200

    added base metods in class Person

--- classes/main_classes.py
+++ classes/main_classes.py
@@ -1,2 +1,15 @@
 class Person:
+    def __init__(self, name, health, attack, defense):
+        self.name = name
+        self.health = health
+        self.attack = attack
+        self.defense = defense
+        
+    def attack_enemy(self, enemy):
+        damage = max(self.attack - enemy.defense, 0)
+        enemy.health = max(enemy.health - damage, 0)
+        
+
+
+if __name__ == "__main__":
     pass
--- main.py
+++ main.py
@@ -1,2 +1,4 @@
+from classes import Person
+
 if __name__ == "__main__":
     pass
