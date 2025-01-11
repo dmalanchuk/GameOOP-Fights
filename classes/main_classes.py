431636092d1e81c3ee767ee06ad66e413a287a28
Daniil Malanchuck <124836521+dmalanchuk@users.noreply.github.com>
2025-01-11 02:08:31 +0200

    created func for fights two players

--- classes/main_classes.py
+++ classes/main_classes.py
@@ -11,7 +11,15 @@ class Person:
         
 
 def fights(player1, player2):
-    pass
+    while player1.health > 0 and player2.health > 0:
+        player1.attack_enemy(player2)
+        player2.attack_enemy(player1)
+    if player1.health == 0:
+        print(f"{player2.name} wins! Remaining health: {player2.health}")
+    else:
+        print(f"{player1.name} wins! Remaining health: {player1.health}")
 
 if __name__ == "__main__":
-    pass
+    john = Person("John", 100, 20, 10)
+    alice = Person("Alice", 100, 35, 5)
+    fights(john, alice)
